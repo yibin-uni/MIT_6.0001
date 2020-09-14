ac065121a894726b50c91496c5dc1efe38a6037e
Yibin Xiong <xyb_1010@yahoo.com>
2020-09-14 14:52:02 +0800

    finish setup

---
+++ ps0/ps0.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 15 10:12:15 2019
+
+@author: yibin
+"""
+
+# In[1]
+x=int(input("enter a number x"))
+y=int(input("enter a number y"))
+exp= x**y
+import numpy
+log_x= numpy.log2(x)
+print(exp, log_x)
+
+# In[2]
+
+x=float(input("enter a number x"))
+y=float(input("enter a number y"))
+exp= x**y
+import numpy
+log_x= numpy.log2(x)
+print(exp, log_x)
+
